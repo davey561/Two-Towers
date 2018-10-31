d611ed9d380af2594ee6248efc5550fae0a1fb1e
Davey <davey561@gmail.com>
2018-10-31 12:18:49 -0400

    Added characteriterator from cpu

--- CharacterIterator.java
+++ CharacterIterator.java
@@ -1,12 +1,29 @@
 import structure5.*;
-
-/**
- * An iterator that iterates over the characters of a String
- */
-public class CharacterIterator extends AbstractIterator<Character> {
-    public CharacterIterator(String str) { }
-    public Character next() { return ' '; }
-    public boolean hasNext() { return false; }
-    public void reset() { }
-    public Character get() { return ' '; }
+class CharacterIterator extends AbstractIterator<Character>{
+  char [] chararr;
+  int temp_index;
+  public CharacterIterator(String str) {
+    super();
+    chararr = str.toCharArray();
+    temp_index = 0;
+  }
+  public Character next() {
+    Assert.pre(hasNext(), "Running next without next element.");
+    temp_index++;
+    return chararr[temp_index-1];
+  }
+  public boolean hasNext() {
+    return (temp_index<chararr.length);
+  }
+  public void reset(){temp_index = 0;}
+  public Character get() {
+    return chararr[temp_index];
+  }
+  public static void main(String[] args) {
+    CharacterIterator it = new CharacterIterator("Davey");
+    while(it.hasNext()){
+      System.out.print(it.next() + " ");
+    }
+    System.out.println("\nExpect D a v e y ");
+  }
 }
